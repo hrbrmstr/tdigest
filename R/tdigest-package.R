a71c712070bbb0cc0e3e74db7b3f469a0efac53d
boB Rudis <bob@rud.is>
2019-02-15 08:48:01 -0500

    R package repo initialization complete

---
+++ R/tdigest-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/tdigest>
+#' - BugReports: <https://gitlab.com/hrbrmstr/tdigest/issues>
+#' 
+#' @md
+#' @name tdigest
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
